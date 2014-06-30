164a6a70628f6fb0e230ed02b20e2dcfea240db9
thedarkginger <jp.gooderham@gmail.com>
2014-06-29 23:38:06 -0400

    added navbar and installed boot

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Topdog::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
+  get "legal" => "pages#legal"
+  get "contact" => "pages#contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
