91ea8330e15157cf0cea1dee4c809fdfc94c9cd2
thedarkginger <jp.gooderham@gmail.com>
2014-06-29 22:55:22 -0400

    trying again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Topdog::Application.config.secret_key_base = '6af7d2242e814b8b1b6490bf438a562b99d56c8ec520649a8305f09ed17b83161692912cc3b544a1d1f4ccb9065bb28ee32b03b21d1daf94df722c3f7e35c2d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Topdog::Application.config.session_store :cookie_store, key: '_topdog_session'
