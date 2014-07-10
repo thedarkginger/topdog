752276027b1a62b9f26c14902d6b7a028af9cf1b
thedarkginger <jp.gooderham@gmail.com>
2014-07-09 23:17:54 -0400

    added more devise setup and views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Topdog::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #set this to the actual host name !!!!!
+  #required for heroku
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
