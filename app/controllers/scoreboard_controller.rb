bc1badbf5393b56fd372981f9b361c17a92e756b
thedarkginger <jp.gooderham@gmail.com>
2016-02-05 08:31:58 -0800

    fixed mobile button for sports lobby

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -2,7 +2,6 @@ class ScoreboardController < ApplicationController
   def index
 
   	 @participations = Participation.where(finished: true, category: session[:category]).order(score: :desc)
-  	 #@chipzs = Chipz.where(category: session[:category])
   end
 
 
