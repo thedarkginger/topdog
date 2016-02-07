f9139a1b967b9a5807e267cf4705fab61f2a99dd
thedarkginger <jp.gooderham@gmail.com>
2016-02-06 23:03:08 -0800

    added city to users, edit access, added to scoreboard

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160205084822) do
+ActiveRecord::Schema.define(version: 20160207064433) do
 
   create_table "chips", force: true do |t|
     t.string   "category"
@@ -149,6 +149,7 @@ ActiveRecord::Schema.define(version: 20160205084822) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
+    t.string   "city"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
