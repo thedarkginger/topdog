49a6fbce2cd51ba051ed6eba031a2c8c91e83047
thedarkginger <jp.gooderham@gmail.com>
2016-02-18 23:38:22 -0800

    made prizes dynamic in scoreboard

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -1,7 +1,7 @@
 class ScoreboardController < ApplicationController
   def index
-
-  	 @top_ten = Participation.where(finished: true, category: session[:category]).order(score: :desc).limit(10)
+  	 @participations = Participation.where(finished: true, category: session[:category]).order(score: :desc).limit(10)
+  	 @quizzes = Quiz.where(category: params[:category])
   end
 
 
