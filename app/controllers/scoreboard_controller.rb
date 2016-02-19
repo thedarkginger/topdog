b2adfc59b015e4ccd6f54ac006884104be3b4ef6
thedarkginger <jp.gooderham@gmail.com>
2016-02-18 23:23:44 -0800

    lobby timer placeholder removes after game starts

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -1,7 +1,7 @@
 class ScoreboardController < ApplicationController
   def index
 
-  	 @participations = Participation.where(finished: true, category: session[:category]).order(score: :desc)
+  	 @top_ten = Participation.where(finished: true, category: session[:category]).order(score: :desc).limit(10)
   end
 
 
