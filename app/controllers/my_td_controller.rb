c6b43d78ae379af25a5fd23d9df69dd09ee1cd4e
thedarkginger <jp.gooderham@gmail.com>
2016-02-21 12:40:30 -0800

    updated my td table to filter non zero scores

--- app/controllers/my_td_controller.rb
+++ app/controllers/my_td_controller.rb
@@ -1,6 +1,7 @@
 class MyTdController < ApplicationController
 
 def index
-	@participations = Participation.where(user_id: current_user.id).order(score: :desc)
+	@participations = Participation.where(user_id: current_user.id).where("score > 0").order(score: :desc)
+	@reservations = Quiz.where("game_start >= ?", Time.now).order(game_start: :asc).limit(4) and Reservation.where (user_id: current_user.id)
 end
 end
