cd7e1f8b90ab5048b6a0dffb4b6d297ab2879f8f
thedarkginger <jp.gooderham@gmail.com>
2016-02-23 00:22:59 -0800

    created my top registered game view

--- app/controllers/my_td_controller.rb
+++ app/controllers/my_td_controller.rb
@@ -2,7 +2,7 @@ class MyTdController < ApplicationController
 
 def index
 	@participations = Participation.where(user_id: current_user.id).where("score > 0").order(score: :desc)
-	@reservations = Reservation.where(user_id: current_user.id)
+	@reservations = Reservation.where(user_id: current_user.id) && Quiz.where("game_start > ?", Time.now).order(game_start: :asc)
 end
 end
 
