4d9e113242f2055aebf65533b11efe8b1841e57d
thedarkginger <jp.gooderham@gmail.com>
2016-02-22 23:34:16 -0800

    updated home page to show games for 2 min after time elapses

--- app/controllers/my_td_controller.rb
+++ app/controllers/my_td_controller.rb
@@ -3,7 +3,6 @@ class MyTdController < ApplicationController
 def index
 	@participations = Participation.where(user_id: current_user.id).where("score > 0").order(score: :desc)
 	@reservations = Reservation.where(user_id: current_user.id)
-	@quizzes = Quiz.where("game_start >= ?", Time.now).order(game_start: :asc)
 end
 end
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-     @quizzes = Quiz.where("game_start >= ?", Time.now).order(game_start: :asc).limit(4)
+     @quizzes = Quiz.where("game_start >= ?", Time.now - 2.minutes).order(game_start: :asc).limit(4)
   end
 
   def testhome
