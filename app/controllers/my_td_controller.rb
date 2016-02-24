e69c3e131bfff954fa4cde444fe58cb084f8a28b
thedarkginger <jp.gooderham@gmail.com>
2016-02-23 18:39:34 -0800

    saving before testing polymorph

--- app/controllers/my_td_controller.rb
+++ app/controllers/my_td_controller.rb
@@ -2,7 +2,7 @@ class MyTdController < ApplicationController
 
 def index
 	@participations = Participation.where(user_id: current_user.id).where("score > 0").order(score: :desc)
-	@reservations = Reservation.where(user_id: current_user.id) && Quiz.where("game_start > ?", Time.now).order(game_start: :asc)
+	@reservations = Reservation.where(user_id: current_user.id).exists? && Quiz.where("game_start > ?", Time.now)
 end
 end
 
