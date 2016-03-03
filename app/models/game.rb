7d7c6eb6af535b0ad46b167e9087715c5ae2a216
sheamf <miles_shea@yahoo.com>
2016-03-02 20:55:35 -0700

    add points allocation model and associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,7 @@ class Game < ActiveRecord::Base
   
   has_many :participations
   has_many :users, through: :participations
+  has_many :points_allocations
   belongs_to :quiz
 
 end
---
+++ app/models/points_allocation.rb
@@ -0,0 +1,5 @@
+class PointsAllocation < ActiveRecord::Base
+
+  belongs_to :game
+
+end
