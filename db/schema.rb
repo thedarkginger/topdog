79453c098eae10bd90f1902d4adba0b80c2f863b
thedarkginger <jp.gooderham@gmail.com>
2016-03-09 00:16:43 -0800

    added confirmable to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302224026) do
+ActiveRecord::Schema.define(version: 20160309081216) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
@@ -124,8 +124,12 @@ ActiveRecord::Schema.define(version: 20160302224026) do
     t.datetime "updated_at"
     t.string   "username"
     t.string   "city"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["username"], name: "index_users_on_username", unique: true
