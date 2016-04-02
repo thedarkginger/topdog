74a9be2b7d7f9f6e74f723f63c337aa3c79205c7
thedarkginger <jp.gooderham@gmail.com>
2016-04-01 17:20:43 -0700

    form input for true/false

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -81,6 +81,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:question_text, :category_id, :correct_answer, :answers_attributes => [:id, :answer_text])
+      params.require(:question).permit(:question_text, :category_id, :correct_answer, :answers_attributes => [:id, :answer_text, :correct_answer])
     end
 end
