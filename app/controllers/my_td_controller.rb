75fda8603c0b981f3730fa8ea86cfef6046654b1
thedarkginger <jp.gooderham@gmail.com>
2016-04-02 18:43:41 -0700

    added my td test - incomplete

--- app/controllers/my_td_controller.rb
+++ app/controllers/my_td_controller.rb
@@ -1,8 +1,8 @@
 class MyTdController < ApplicationController
 
 def index
-	@participations = Participation.where(user_id: current_user.id).where("score > 0").order(score: :desc)
-	@reservations = Reservation.where(user_id: current_user.id).exists? && Quiz.where("game_start > ?", Time.now)
+	# @participations = Participation.where(user_id: current_user.id).where("score > 0").order(score: :desc)
+	@participations = Participation.where(user_id: current_user.id).exists? && Game.where("starts_at > ?", Time.now)
 end
 end
 
