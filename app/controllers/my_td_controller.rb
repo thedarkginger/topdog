6c1c0c04316347fa3aac54622c869fbb53c3790d
thedarkginger <jp.gooderham@gmail.com>
2016-04-03 23:58:56 -0700

    fixed mytopdog - no score info yet

--- app/controllers/my_td_controller.rb
+++ app/controllers/my_td_controller.rb
@@ -2,7 +2,7 @@ class MyTdController < ApplicationController
 
 def index
 	# @participations = Participation.where(user_id: current_user.id).where("score > 0").order(score: :desc)
-	@participations = Participation.where(user_id: current_user.id).exists? && Game.where("starts_at > ?", Time.now)
+	@games = Participation.where(user_id: current_user.id).exists? && Game.where("starts_at > ?", Time.now)
 end
 end
 
