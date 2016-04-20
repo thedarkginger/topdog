2e3bd90c84d96973ca6638a36679143ab29a6121
thedarkginger <jp.gooderham@gmail.com>
2016-04-19 21:59:32 -0700

    updated scoreboard

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -3,13 +3,13 @@ class ScoreboardController < ApplicationController
 
     # instance variable for the view, ignore 
   	@participations = Participation.where(finished: true, game_id: session[:game_id]).where("score > ?", 0).order(score: :desc).limit(10)
-  	@game = Game.find(params[:game_id])
+  	@game = Game.where(id: params[:game_id])
     @firstpoints = PointsAllocation.where(game_id: params[:game_id]).where(place: "1").pluck(:points).map(&:to_i).first
     @secondpoints = PointsAllocation.where(game_id: params[:game_id]).where(place: "2").pluck(:points).map(&:to_i).first
     @thirdpoints = PointsAllocation.where(game_id: params[:game_id]).where(place: "3").pluck(:points).map(&:to_i).first 
 
     unless Ranking.where(game_id: params[:game_id]).present?
-        Delayed::Job.enqueue(ChipsUpdaterJob.new(@game.id), :run_at => @game.starts_at + 3.minutes)
+        Delayed::Job.enqueue(ChipsUpdaterJob.new(@game.id), :run_at => @game.starts_at + 7.minutes)
     end 
 
 end
